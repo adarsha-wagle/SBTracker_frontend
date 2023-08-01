eb08d9d6961295e7231eb8cc32fccbeb184a7af0
Iamnoob77 <68275507+Iamnoob77@users.noreply.github.com>
2023-08-01 12:43:41 +0545

    added map and integrated

---
+++ src/component/customIcon.jsx
@@ -0,0 +1,8 @@
+import L from "leaflet";
+import reactIcon from "../assets/reactIcon.png";
+export const markerIcon = new L.Icon({
+  iconUrl: reactIcon,
+  iconRetinaUrl: reactIcon,
+  popupAnchor: [-0, -0],
+  iconSize: [20, 20],
+});
